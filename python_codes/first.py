5de4b5facab8ea322bba21cbc4d77b1edbbb9aa3
varathaneie <varathaneie@gmail.com>
2023-02-14 19:28:28 +0530

    Updated first.py

--- python_codes/first.py
+++ python_codes/first.py
@@ -1,7 +1,7 @@
 print("Hello World")
 print("lets check a question")
-indata=input("Are you hungry? - ")
-if indata=="yes":
+indata = input("Are you hungry? - ")
+if indata == "yes":
     print("try to have some food")
 else:
     print("All good i presume")
